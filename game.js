b1d0c90a74c40d64b299417a4841cc860bd152fb
Enrique1311 <ejspinelli131176@gmail.com>
2023-03-07 23:47:50 -0300

    The LocalStorage for record was edded

--- game.js
+++ game.js
@@ -4,10 +4,18 @@ const buttonUp = document.querySelector("#up");
 const buttonDown = document.querySelector("#down");
 const buttonRight = document.querySelector("#right");
 const buttonLeft = document.querySelector("#left");
+const livesSpan = document.querySelector("#lives");
+const timeSpan = document.querySelector("#time");
+const spanRecord = document.querySelector("#record");
+const pResult = document.querySelector("#result");
 
 let canvasSize;
 let elementsSize;
 let level = 0;
+let lives = 3;
+let timeStart;
+let timePlayer;
+let timeInterval;
 
 const playerPosition = {
 	x: undefined,
@@ -51,9 +59,17 @@ function startGame() {
 		return;
 	}
 
+	if (!timeStart) {
+		timeStart = Date.now();
+		timeInterval = setInterval(showTimer, 1000);
+		showRecord();
+	}
+
 	const mapRows = map.trim().split("\n"); // con trim()con split() crea un array a partir de un strig separado por los espacios vacíos(\n)
 	const mapRowColumns = mapRows.map((row) => row.trim().split("")); // creamos un array de arrays(filas) con arrays de las columnas
 	console.log({ map, mapRows, mapRowColumns });
+
+	showLives();
 
 	bombsPosition = [];
 
@@ -62,7 +78,7 @@ function startGame() {
 	mapRowColumns.forEach((row, rowIndex) => {
 		row.forEach((col, colIndex) => {
 			const emoji = emojis[col];
-			const xPos = elementsSize * (colIndex + 1) + elementsSize / 6;
+			const xPos = elementsSize * (colIndex + 1) + elementsSize / 5;
 			const yPos = elementsSize * (rowIndex + 1) - elementsSize / 7;
 
 			if (col == "O") {
@@ -103,7 +119,7 @@ function movePlayer() {
 	});
 
 	if (bombCrash) {
-		console.log("Bomba");
+		levelFailed();
 	}
 
 	gameContext.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
@@ -115,8 +131,55 @@ function levelWinner() {
 	startGame();
 }
 
+function levelFailed() {
+	lives--;
+	console.log(lives);
+
+	if (lives <= 0) {
+		level = 0;
+		lives = 3;
+		timeStart = undefined;
+	}
+	playerPosition.x = undefined;
+	playerPosition.y = undefined;
+	startGame();
+}
+
+function showLives() {
+	const heartsArray = Array(lives).fill(emojis["HEART"]);
+	console.log(heartsArray);
+	livesSpan.innerHTML = "";
+	heartsArray.forEach((heart) => livesSpan.append(heart));
+}
+
+function showTimer() {
+	timeSpan.innerHTML = Date.now() - timeStart;
+}
+
+function showRecord() {
+	spanRecord.innerHTML = localStorage.getItem("record_time");
+}
+
 function gameWinner() {
 	console.log("GANASTEEEEE!!!!!!");
+	clearInterval(timeInterval);
+
+	const recordTime = localStorage.getItem("record_time");
+	const playerTime = Date.now() - timeStart;
+
+	if (recordTime) {
+		if (playerTime <= recordTime) {
+			localStorage.setItem("record_time", playerTime);
+			pResult.innerHTML = "¡Felicidades! ¡Superáste el record!";
+		} else {
+			pResult.innerHTML = "Lo siento😔... No superáste el record...";
+		}
+	} else {
+		localStorage.setItem("record_time", playerTime);
+		pResult.innerHTML =
+			"Como es la primera vez que juegas, tienes el record...👍";
+	}
+	console.log({ recordTime, playerTime });
 }
 
 window.addEventListener("keydown", moveByKeys);
